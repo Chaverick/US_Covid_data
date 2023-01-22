35a26b211f7d67474e75ce116707ff18db50b0c0
Chaverick <123317534+Chaverick@users.noreply.github.com>
2023-01-22 10:05:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ async function fetchData() {
     const res=await fetch ("https://api.covidactnow.org/v2/country/US.json?apiKey=f86424932eb44ac7bdbe0e2cdde14841");
     const record=await res.json();
     //document.getElementById("date");
-    document.getElementById("country");
     document.getElementById("metrics.weeklyNewCasesPer100k");
     document.getElementById("actuals.deaths");
 }
